4f13c6134ce96d937d73c29cf6d48c5e12b6b19b
Piotr Bajger <piotr.bajger@hotmail.com>
2020-10-10 14:37:30 +0200

    Rename files, name complex types

--- src/count_vocab.rs
+++ src/count_vocab.rs
@@ -4,9 +4,13 @@ use regex::Regex;
 use std::collections::HashMap;
 
 
+pub type Vocabulary = HashMap<String, i64>;
+pub type CsrMatrix = (Vec<i64>, Vec<i64>, Vec<usize>);
+
+
 pub fn count_vocab(
     raw_documents: Vec<String>, lower: bool, ngram_range: (usize, usize)
-) -> (HashMap<String, i64>, (Vec<i64>, Vec<i64>, Vec<usize>)) {
+) -> (Vocabulary, CsrMatrix) {
     let re = Regex::new(r"(?u)\b\w\w+\b").unwrap();
 
     let mut vocabulary: HashMap<String, i64> = HashMap::new();
@@ -22,7 +26,7 @@ pub fn count_vocab(
     let mut temp_token: &str;
 
     for doc in raw_documents.iter() {
-        let mut counter: HashMap<String, i64> = HashMap::new();
+        let mut counter: HashMap<i64, i64> = HashMap::new();
         let doc: String = if lower { doc.to_lowercase() } else { doc.into() };
         let tokenizer = Tokenizer::new(&doc, ngram_range, &re);
 
@@ -33,15 +37,15 @@ pub fn count_vocab(
                 vocabulary.insert(temp_token.to_string(), word_count);
                 word_count += 1;
             }
+            let token_index = vocabulary.get(&cap).unwrap();
             // Increase word count in current document
-            *counter.entry(temp_token.to_string()).or_insert(0) += 1;
+            *counter.entry(*token_index).or_insert(0) += 1;
         }
 
         // Create next CSR matrix row
-        for (token, count) in counter.iter() {
-            data.push(*count);
-            indices.push(*vocabulary.get(token).unwrap());
-        }
+        let fixed_counter: Vec<(&i64, &i64)> = counter.iter().collect();
+        data.extend(fixed_counter.iter().map(|p| p.1));
+        indices.extend(fixed_counter.iter().map(|p| p.0));
         indptr.push(data.len());
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
 use pyo3::prelude::*;
 use pyo3::types::*;
 
 mod tokenizer;
-mod count_vectorizer;
+mod count_vocab;
 
 
 #[pymodule]
@@ -11,10 +10,13 @@ fn nlplease(_py: Python, m: &PyModule) -> PyResult<()> {
 
     #[pyfn(m, "count_vocab")]
     fn count_vocab(
-        _py: Python, py_raw_documents: &PyList, lower: bool, ngram_range: (usize, usize)
-    ) -> (HashMap<String, i64>, (Vec<i64>, Vec<i64>, Vec<usize>)) {
+        _py: Python,
+        py_raw_documents: &PyList,
+        lower: bool,
+        ngram_range: (usize, usize)
+    ) -> (count_vocab::Vocabulary, count_vocab::CsrMatrix) {
         let raw_documents: Vec<String> = py_raw_documents.extract().unwrap();
-        count_vectorizer::count_vocab(raw_documents, lower, ngram_range)
+        count_vocab::count_vocab(raw_documents, lower, ngram_range)
     }
 
     Ok(())
